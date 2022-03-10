f611c65bf758e77487c4a37a9d820114968f7a8d
Jerry Nolf <nolf.jerry@gmail.com>
2022-03-10 10:13:39 -0600

    updated and ready

--- acquire.py
+++ acquire.py
@@ -6,13 +6,13 @@ import env
 
 
 def get_telco_data():
-
+#Define filename
     filename = 'telco.csv'
-    
+#Create function    
     if os.path.exists(filename):
         print('Reading from csv file...')
         return pd.read_csv(filename)
-
+# Define parameters
     database = 'telco_churn'     
     get_db_url = f'mysql+pymysql://{env.user}:{env.password}@{env.host}/'
     query = '''
@@ -21,11 +21,10 @@ def get_telco_data():
             JOIN contract_types USING(contract_type_id)
             JOIN internet_service_types USING(internet_service_type_id)
             JOIN payment_types USING(payment_type_id)
-            '''
-     
-    df = pd.read_sql(query, url)
+            '''     
+    df = pd.read_sql(query, get_db_url)
     df.to_csv(filename, index=False)
-
+    
     return df
 
 
