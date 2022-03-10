9180695993014e0b3074a66f0866d7c4308d343a
Jerry Nolf <nolf.jerry@gmail.com>
2022-03-10 10:13:54 -0600

    updated and ready

--- prepare.py
+++ prepare.py
@@ -6,35 +6,36 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 
-def prep_telco(telco_df):
+def prep_telco(df):
     '''
         Function takes in the telco dataset and readies it for use ensuring all
-        categories are 
+        categories are usable for analysis
     '''
-    telco_df = telco_df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id'])
-    
-    telco_df.total_charges = telco_df.total_charges.replace(' ', 0).astype(float)
-    
+# Drop unnecessary columns that contain dupilicated information    
+    df = df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'])
+# Change the Total Changes column to a float    
+    df.total_charges = df.total_charges.replace(' ', 0).astype(float)
+# Identify columns to be receive dummies   
     cat_columns = ['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing', 'churn', 'contract_type', 'internet_service_type', 'payment_type']
-
+# Create dummies
     for col in cat_columns:
-        telco_dummy = pd.get_dummies(telco_df[col],
-                                     prefix=telco_df[col].name,
+        dummies = pd.get_dummies(df[col],
+                                     prefix=df[col].name,
                                      dummy_na=False,
                                      drop_first = True)
-        telco_df = pd.concat([telco_df, telco_dummy], axis=1)
-        telco_df = telco_df.drop(columns=[col])    
-    return telco_df 
+        df = pd.concat([df, dummies], axis=1)
+        df = df.drop(columns=[col])    
+    return df
 
-def train_validate_test_split(df, target, seed=123):
+# Create a TVT split
+def train_validate_test_split(df, target, seed=1349):
     '''
-    This function takes in a dataframe, the name of the target variable
-    (for stratification purposes), and an integer for a setting a seed
-    and splits the data into train, validate and test. 
-    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
-    original dataset, and train is .70*.80= 56% of the original dataset. 
-    The function returns, in this order, train, validate and test dataframes. 
-    '''
+    This function takes in a dataframe and splits it into train, validate, and test. 
+    Test is first extracted as 20% test, 80% train_validate split.
+    Then from the remaining 80%, the train and validate are extracted using a 70/30
+    split respectively. The function then returns them in the order of train,
+    validate, and test.
+    '''   
     train_validate, test = train_test_split(df, test_size=0.2, 
                                             random_state=seed, 
                                             stratify=df[target])
